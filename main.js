8d186c123bafb3b727ebed1d93bb0cedd1471444
alexisraya <aar335@drexel.edu>
2022-02-26 17:21:05 -0500

    :bomb: deleted old files

---
+++ main.js
@@ -0,0 +1,8 @@
+//VARIABLES
+const projects = document.querySelector('js-projects');
+const proj1 = document.querySelector('js-proj1');
+const proj2 = document.querySelector('js-proj2');
+const proj3 = document.querySelector('js-proj3');
+const proj4 = document.querySelector('js-proj4');
+
+// When the projects come into view, I want them to fade in
