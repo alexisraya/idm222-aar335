dde82e41a5c602b4d10db8443ac4cc76147be0c6
alexisraya <aar335@drexel.edu>
2022-03-13 20:18:16 -0400

    :zap: clean up js, reduce erros

--- main.js
+++ main.js
@@ -1,49 +1,54 @@
 //VARIABLES
-const projects = document.querySelector('.js-projects');
-const proj1 = document.querySelector('.js-proj1');
-const proj2 = document.querySelector('.js-proj2');
-const proj3 = document.querySelector('.js-proj3');
-const proj4 = document.querySelector('.js-proj4');
+const windowPath = window.location.pathname;
+if (windowPath.includes("index") || windowPath.includes("case-studies") ){
+    const projects = document.querySelector('.js-projects');
+    const proj1 = document.querySelector('.js-proj1');
+    const proj2 = document.querySelector('.js-proj2');
+    const proj3 = document.querySelector('.js-proj3');
+    const proj4 = document.querySelector('.js-proj4');
+    
+    window.addEventListener("scroll", function () {
+        applyClassWhenInView(projects, 'fadeIn');
+        applyClassWhenInView(proj1, 'fadeIn');
+        applyClassWhenInView(proj2, 'fadeIn');
+        applyClassWhenInView(proj3, 'fadeIn');
+        applyClassWhenInView(proj4, 'fadeIn');
+    
+    });
+}
 
-const gal1 = document.querySelector('.js-gallery1');
-const gal2 = document.querySelector('.js-gallery2');
-const gal3 = document.querySelector('.js-gallery3');
-const gal4 = document.querySelector('.js-gallery4');
-const gal5 = document.querySelector('.js-gallery5');
-const gal6 = document.querySelector('.js-gallery6');
-const gal7 = document.querySelector('.js-gallery7');
-const gal8 = document.querySelector('.js-gallery8');
-const gal9 = document.querySelector('.js-gallery9');
-const gal10 = document.querySelector('.js-gallery10');
-const gal11 = document.querySelector('.js-gallery11');
-const gal12 = document.querySelector('.js-gallery12');
+if (windowPath.includes("creative")){
+    const gal1 = document.querySelector('.js-gallery1');
+    const gal2 = document.querySelector('.js-gallery2');
+    const gal3 = document.querySelector('.js-gallery3');
+    const gal4 = document.querySelector('.js-gallery4');
+    const gal5 = document.querySelector('.js-gallery5');
+    const gal6 = document.querySelector('.js-gallery6');
+    const gal7 = document.querySelector('.js-gallery7');
+    const gal8 = document.querySelector('.js-gallery8');
+    const gal9 = document.querySelector('.js-gallery9');
+    const gal10 = document.querySelector('.js-gallery10');
+    const gal11 = document.querySelector('.js-gallery11');
+    const gal12 = document.querySelector('.js-gallery12');
 
-// When the projects come into view, I want them to fade in
+    // When the projects come into view, I want them to fade in
 
-window.addEventListener("scroll", function () {
-    applyClassWhenInView(projects, 'fadeIn');
-    applyClassWhenInView(proj1, 'fadeIn');
-    applyClassWhenInView(proj2, 'fadeIn');
-    applyClassWhenInView(proj3, 'fadeIn');
-    applyClassWhenInView(proj4, 'fadeIn');
 
-});
-
-window.addEventListener("scroll", function () {
-    applyClassWhenInView(gal1, 'fadeIn');
-    applyClassWhenInView(gal2, 'fadeIn');
-    applyClassWhenInView(gal3, 'fadeIn');
-    applyClassWhenInView(gal4, 'fadeIn');
-    applyClassWhenInView(gal5, 'fadeIn');
-    applyClassWhenInView(gal6, 'fadeIn');
-    applyClassWhenInView(gal7, 'fadeIn');
-    applyClassWhenInView(gal8, 'fadeIn');
-    applyClassWhenInView(gal9, 'fadeIn');
-    applyClassWhenInView(gal10, 'fadeIn');
-    applyClassWhenInView(gal11, 'fadeIn');
-    applyClassWhenInView(gal12, 'fadeIn');
-});
-
+    window.addEventListener("scroll", function () {
+        applyClassWhenInView(gal1, 'fadeIn');
+        applyClassWhenInView(gal2, 'fadeIn');
+        applyClassWhenInView(gal3, 'fadeIn');
+        applyClassWhenInView(gal4, 'fadeIn');
+        applyClassWhenInView(gal5, 'fadeIn');
+        applyClassWhenInView(gal6, 'fadeIn');
+        applyClassWhenInView(gal7, 'fadeIn');
+        applyClassWhenInView(gal8, 'fadeIn');
+        applyClassWhenInView(gal9, 'fadeIn');
+        applyClassWhenInView(gal10, 'fadeIn');
+        applyClassWhenInView(gal11, 'fadeIn');
+        applyClassWhenInView(gal12, 'fadeIn');
+    });
+}
 // Break this out to be reusable
 function applyClassWhenInView(
     // Element to watch
